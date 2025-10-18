11c67811b7b65982f0fd4e90ad1ad1c1aa4a1daf
Ergi Zenila Marta Sasmige <ergizms@gmail.com>
2025-10-18 18:37:01 +0700

    PUSH MIAW

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/dashboard', [AuthController::class, 'dashboard'])->middleware('auth')->name('dashboard');
 Route::get('/add-admin', [AuthController::class, 'addAdmin'])->middleware('auth')->name('addAdmin');
 Route::post('/add-admin', [AuthController::class, 'storeAdmin'])->middleware('auth')->name('superadmin.addAdmin.submit');
+
